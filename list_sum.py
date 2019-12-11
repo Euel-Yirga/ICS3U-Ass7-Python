324281ce8fff41cb515e333d5dcddd9c743eaa35
Ubuntu <ubuntu@ip-172-31-30-141.ec2.internal>
2019-12-11 00:08:19 +0000

    sum ofnumbers in list

---
+++ list_sum.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Euel Yirga
+# Created on: Dec 2019
+# omputes the sum of numbers in a list
+list1 = [100, 200, 300]
+
+total = sum(list1)
+
+print("Sum of all elements in given list: ", total)
